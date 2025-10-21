cd2b588c47ba7d254a3721cd0b26d1336fc9fddd
roxane-hub <marielleroxane037@gmail.com>
2025-10-21 09:59:37 +0300

    this is my project

---
+++ src/main/java/com/example/websorting/WebsortingApplication.java
@@ -0,0 +1,13 @@
+package com.example.websorting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsortingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsortingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/websorting/WebsortingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.websorting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsortingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
